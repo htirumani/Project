bbcab2d8e948b6c410fb796a7ac36746b5e2241c
AVP Neelam <neelam.akula@gmail.com>
2021-06-09 12:06:02 -0400

    doc formatting & push to DB

---
+++ Data Collection/Fitbit/README.md
@@ -0,0 +1,52 @@
+# Fitbit
+
+Utilizes [Fitbit unofficial API](https://github.com/orcasgit/python-fitbit) to fetch user data and format JSON docs for every minute, adapted from an [article](https://towardsdatascience.com/using-the-fitbit-web-api-with-python-f29f119621ea) by Michael Galarnyk. Then it pushes these docs to the relevant collections in the MongoDB Atlas database. The script currently calls and pushes minute data for sleep and heart rate data.
+
+## Using Script
+
+---
+
+Calling the script requires two command line arguments with an optional third argument. The first argument is the date of the data to be collected, this is in YYYY-MM-DD format. The second argument is the userID which is written in all docs pushed to the MongoDB database. The last (optional) argument chooses whether or not the API calls are saved in an external 'output' folder.
+
+### Example Calls
+* `python3 script.py 2021-06-01 1`
+* `python3 script.py 2021-06-01 1 y`
+
+## Example JSON Doc Formating
+
+---
+
+Some quick notes
+
+* `"user"` field correlates to an integer.
+* `"device"` field correlates to an integer, for this project a `0` represents the Fitbit device and a `1` represents the Samsung device.
+* `"date"` is in YYYY-MM-DD format.
+* For the Sleep docs `"stage"` can be either `1`,`2`, or `3` which directly correlate to "asleep", "awake", and "really awake".
+
+### Sleep
+```json
+{
+	"user" : 1,
+ 	"device" : 0,
+ 	"date" : "2021-06-01",
+ 	"time" : "00:00:00",
+ 	"stage" : 1
+}
+```
+
+### Heartrate
+```json
+{
+	"user" : 1,
+ 	"device" : 0,
+ 	"date" : "2021-06-01",
+ 	"time" : "00:00:00",
+ 	"value" : 60
+}
+```
+
+## MongoDB
+
+---
+
+Connects to the MongoDB database specified in the `ATLAS` field, then the `push_docs(docs, base, feature)` method inserts `docs` to the collection `feature` in the database `base`.
---
+++ Data Collection/Fitbit/script.py
@@ -0,0 +1,113 @@
+"""
+Fetches data off of Fitbit account, adapted from:
+https://towardsdatascience.com/using-the-fitbit-web-api-with-python-f29f119621ea
+
+Using an unofficial api from:
+https://github.com/orcasgit/python-fitbit
+"""
+
+from datetime import datetime
+from pymongo import MongoClient
+import gather_keys_oauth2 as Oauth2
+import fitbit
+import json
+import os
+import sys
+
+# define client id and secret
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+# define MongoDB url
+ATLAS = ''
+
+# write file method
+def write_json(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+# parse body data into appropriate JSON docs
+def parse_body(data, user):
+    date = data.get()
+
+# parse sleep data into appropriate JSON docs
+def parse_sleep(data, user):
+    date = data.get('sleep')[0].get('dateOfSleep')
+    minData = data.get('sleep')[0].get('minuteData')
+    docs = []
+
+    for min in minData: # generate doc using below template
+        doc = {
+            'user' : user,
+            'device' : 0,
+            'date' : date,
+            'minute' : min.get('dateTime')[:-3],
+            'stage' : min.get('value')}
+        docs.append(doc)
+
+    return docs
+
+# parse heart rate data into appropriate JSON docs
+def parse_heart(data, user):
+    date = data.get('activities-heart')[0].get('dateTime')
+    minData = data.get('activities-heart-intraday').get('dataset')
+    docs = []
+    
+    for min in minData: # generate doc using below template
+        doc = {
+            'user' : user,
+            'device' : 0,
+            'date' : date,
+            'minute' : min.get('time')[:-3],
+            'heart_rate' : min.get('value')}
+        docs.append(doc)
+
+    return docs
+
+# pushes all docs in docs to specified collection in MongoDB
+def push_docs(docs, base, feature):
+    client = MongoClient(ATLAS)
+    db = client[base]
+    collection = db[feature]
+    collection.insert_many(docs)
+
+# main runner method
+def runner(date, user, saveData):
+    # authorize API calls
+    server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+    server.browser_authorize()
+    ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+    REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+    auth2_client = fitbit.Fitbit(CLIENT_ID,CLIENT_SECRET,oauth2=True,access_token=ACCESS_TOKEN,refresh_token=REFRESH_TOKEN)
+    day = date
+
+    # get data from API calls
+    data_body = auth2_client.body(day)
+    data_sleep = auth2_client.sleep(day)
+    data_heart = auth2_client.intraday_time_series('activities/heart', day)
+
+    # create relevant docs from API data
+    sleep_docs = parse_sleep(data_sleep, user)
+    heart_docs = parse_heart(data_heart, user)
+
+    # save data to external JSON files
+    if saveData:
+        new_dir = os.getcwd() + '/output/' + day
+        os.mkdir(new_dir)
+        write_json(data_body, new_dir+'/body.json')
+        write_json(data_sleep, new_dir+'/sleep.json')
+        write_json(data_heart, new_dir+'/heart.json')
+
+    # push docs to MongoDB
+    push_docs(sleep_docs, 'test', 'sleep')
+    push_docs(heart_docs, 'test', 'heartrate')
+
+# Verifies arguments
+if __name__ == "__main__":
+    print(str(sys.argv))
+    if len(sys.argv) == 3:
+        runner(sys.argv[1], sys.argv[2], False)
+    elif len(sys.argv) == 4 and sys.argv[3] == 'y':
+        runner(sys.argv[1], sys.argv[2], True)
+    else:
+        print("Invalid number of arguments, check documentation.")
