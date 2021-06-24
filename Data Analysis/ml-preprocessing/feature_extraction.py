a8b7965ec710a713dcb6908a8972812cee7896ce
maxkleinsasser <maxwell.kleinsasser@gmail.com>
2021-06-24 15:29:48 -0500

    small ML script updates

--- Data Analysis/ml-preprocessing/feature_extraction.py
+++ Data Analysis/ml-preprocessing/feature_extraction.py
@@ -7,9 +7,18 @@ from matplotlib import pyplot as plt
 path = 'Data Analysis/processed-data-files/all_df_clean.csv'
 df = pd.read_csv(path, index_col=0, parse_dates=['DATE'])
 
-times = df['DATE'].to_list()
-hours = [int(t.hour < 9 or t.hour > 21) for t in times]
+'''
+0 for 9am-9pm, 1 else
+'''
+def append_nighttime_feature(df):
+    times = df['DATE'].to_list()
+    hours = [int(t.hour < 9 or t.hour > 21) for t in times]
 
-# 0 for 9am-9pm, 1 else
-df['NIGHTTIME'] = hours
-df.to_csv('user1_featured.csv')
+    df['NIGHTTIME'] = hours
+
+'''
+appends both MEAN_{}MIN_HR and SD_{}MIN_HR
+corresponding to mean and sd of HR data for previous 'window' minutes
+'''
+def append_historical_hr_features(window):
+    pass
