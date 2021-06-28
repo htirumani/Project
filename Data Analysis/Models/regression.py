513705010ba7d788636fddfb144fdfddfa05152b
maxkleinsasser <maxwell.kleinsasser@gmail.com>
2021-06-28 16:09:35 -0500

    Add hist. HR to feature_extraction & add to csvs

--- Data Analysis/Models/regression.py
+++ Data Analysis/Models/regression.py
@@ -3,17 +3,18 @@ import pandas as pd
 import datetime
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 from utils import custom_train_test_split
 
 from pprint import pprint
 
-u0_path = 'Data Analysis/Dataset/clean/user0_df_clean.csv'
-u2_path = 'Data Analysis/Dataset/clean/user2_df_clean.csv'
-
+u0_path = 'Data Analysis/Dataset/final/user0_featured.csv'
+u2_path = 'Data Analysis/Dataset/final/user2_featured.csv'
 split_prop = 0.85
 
-X_train, y_train, X_test, y_test = custom_train_test_split(['HEART', 'STEP'], [u0_path, u2_path], split_prop)
+X_train, y_train, X_test, y_test = custom_train_test_split(['HEART', 'STEP', 'NIGHTTIME', 'MEAN_10MIN_HR', 'SD_10MIN_HR'], [u0_path, u2_path], split_prop)
 
 # define and fit Logistic Regression model
 model = LogisticRegression(random_state=0).fit(X_train, y_train)
@@ -31,7 +32,13 @@ fn = y_test & np.logical_not(y_hat)
 fp = np.logical_not(y_test) & y_hat
 
 print()
-print('Testing Errors: {} / {}'.format(sum(errs), y_hat.shape[0]) )
+print('Confusion Matrix\n', confusion_matrix(y_test, y_hat))
+
+print()
+print('Classification Report\n', classification_report(y_test, y_hat))
+
+print()
+print('Testing Errors: {} / {}'.format(sum(errs), y_hat.shape[0]))
 print('Num False Negatives:', sum(fn))
 print('Num False Positives:', sum(fp))
 
--- Data Analysis/Processing/feature_extraction.py
+++ Data Analysis/Processing/feature_extraction.py
@@ -1,11 +1,39 @@
 from datetime import datetime
+from datetime import timedelta
 import pandas as pd
 import numpy as np
 from pprint import pprint
 from matplotlib import pyplot as plt
 
-path = 'Data Analysis/processed-data-files/all_df_clean.csv'
-df = pd.read_csv(path, index_col=0, parse_dates=['DATE'])
+'''
+Given a clean dataframe FROM A SINGLE USER, returns a list of tuples
+containing start and end times of contiguous chunks of data.
+'''
+def get_contiguous_chunks(df):
+    # get data sorted by date
+    sdf = df.sort_values('DATE', ascending=True)
+    ixs = np.array(list(range(sdf.shape[0])))
+    sdf.set_index(ixs, inplace = True)
+
+    tups = []
+    s = sdf.at[0, 'DATE']
+    p = sdf.at[0, 'DATE']
+    l = 0
+    delta = timedelta(minutes = 1)
+    for index, row in sdf.iterrows():
+        if index == 0: continue
+
+        c = row['DATE']
+        if c != p+delta:
+            tups.append( (s, p, l) )
+            s = c
+            l = 0
+        else:
+            l += 1
+        p = c
+    tups.append( (s, p, l) )
+
+    return tups
 
 '''
 0 for 9am-9pm, 1 else
@@ -19,6 +47,74 @@ def append_nighttime_feature(df):
 '''
 appends both MEAN_{}MIN_HR and SD_{}MIN_HR
 corresponding to mean and sd of HR data for previous 'window' minutes
+
+note: requires the omittion of rows without 'window' minutes of data
+immediately preceding them
 '''
-def append_historical_hr_features(window):
-    pass
+def append_historical_hr_features(df, window):
+    mean_label = 'MEAN_{}MIN_HR'.format(window)
+    sd_label = 'SD_{}MIN_HR'.format(window)
+    
+    df.sort_values('DATE', ascending=True, inplace=True)
+    ixs = np.array(list(range(df.shape[0])))
+    df.set_index(ixs, inplace = True)
+
+    df[mean_label] = None
+    df[sd_label] = None
+
+    for index, row in df.iterrows():
+        
+        # attempt to get row's preceding HR values
+        hist = []
+        punt = False
+        for m in range(1, window+1):
+            if punt: break
+
+            if index - m < 0: 
+                punt = True
+                continue
+
+            curr = df.at[index-m, 'DATE']
+            if curr != row['DATE'] - timedelta(minutes=m): 
+                punt = True
+                continue
+
+            hist.append(df.at[index-m, 'HEART'])
+        
+        # append mean and sd of hist if all data is collected
+        if not punt:
+            df.at[index, mean_label] = np.mean(hist)
+            df.at[index, sd_label] = np.std(hist)
+        
+    df.dropna(inplace=True)
+
+'''
+Takes a list of filepaths, appends extracted features from the data in those files, saves new data to files in 'final' folder.
+
+fps: list of data files on which to append features
+users: list of user indexes corresponding to files in fps
+nighttime...historical_hr: boolean on whether to include said feature
+historical_hr_window: window argument for historical hr feature
+'''
+def append_and_write(fps, users, nighttime = False, historical_hr = False, historical_hr_window = None):
+    for path, user in zip(fps, users):
+        print('Processing user', user)
+        df = pd.read_csv(path, parse_dates=['DATE'])
+
+        if nighttime:
+            print('Appending nighttime...')
+            append_nighttime_feature(df)
+        if historical_hr:
+            print('Appending historical HR...')
+            append_historical_hr_features(df, historical_hr_window)
+
+        print('Writing data...')
+        df.to_csv('Data Analysis/Dataset/final/user{}_featured.csv'.format(user))
+    
+    print('Done')
+
+user0 = 'Data Analysis/Dataset/clean/user0_clean.csv'
+user1 = 'Data Analysis/Dataset/clean/user1_clean.csv'
+user2 = 'Data Analysis/Dataset/clean/user2_clean.csv'
+
+append_and_write([user0, user2], [0, 2], nighttime=True, historical_hr = True, historical_hr_window = 10)
